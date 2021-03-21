17bf75c79b02b5ea6a304b3af8f5b1157e98c169
shakoin <50535453+shakoin@users.noreply.github.com>
2021-03-21 00:38:05 -0600

    Update unloadCouponsFromCard.js
    Updated to run on new site code.

--- unloadCouponsFromCard.js
+++ unloadCouponsFromCard.js
@@ -1,5 +1,5 @@
 var maxCouponsAllowed = 150;
-var unloadCouponFromCardClassName = "kds-Button kds-Button--destructiveSecondary kds-Button--compact CouponButton";
+var unloadCouponFromCardClassName = "kds-Button kds-Button--cancel kds-Button--compact CouponActionButton CouponCard-button";
 for (var i = 0; i < maxCouponsAllowed; i++) {
 	document.getElementsByClassName(unloadCouponFromCardClassName)[i].click();
 	setTimeout(function(){}, 250); //Time out to not overload website.
