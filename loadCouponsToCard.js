202a03cceb4954e8dd63b49a99a6a309fcb36189
shakoin <50535453+shakoin@users.noreply.github.com>
2021-03-21 13:24:01 -0600

    Update loadCouponsToCard.js

--- loadCouponsToCard.js
+++ loadCouponsToCard.js
@@ -1,5 +1,5 @@
 var maxCouponsAllowed = 150;
-var loadCouponToCardClassName = "kds-Button kds-Button--favorable kds-Button--compact CouponButton";
+var loadCouponToCardClassName = "kds-Button kds-Button--primary kds-Button--compact CouponActionButton";
 for (var i = 0; i < maxCouponsAllowed; i++) {
 	document.getElementsByClassName(loadCouponToCardClassName)[i].click();
 	setTimeout(function(){}, 250); //Time out to not overload website.
